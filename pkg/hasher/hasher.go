7812743f0e0adc0854497146b3d51052a7c997c1
Tejas-117 <tejasftw117@gmail.com>
2026-04-02 15:27:58 +0530

    added hasher and embedder functions

--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -1,16 +1,36 @@
 package hasher
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	"io"
+	"os"
+
+	// xxhash library, reference: https://pkg.go.dev/github.com/cespare/xxhash
+	"github.com/cespare/xxhash/v2"
 )
 
-// CalculateHash generates a SHA256 hash of the input string
-func CalculateHash(input string) (string, error) {
-	hash := sha256.Sum256([]byte(input))
-	return hashToString(hash), nil
-}
+/*
+	params:
+		filePath: path of the file as saved in the filesystem
 
-func hashToString(hash [32]byte) string {
-	return hex.EncodeToString(hash[:])
+	return: 
+		a numeric hash of type uint64
+		ex: 6925293637973706096
+*/
+func hashFile(filePath string) (uint64, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	// initialise the hasher
+	hasher := xxhash.New()
+
+	// Copy function copies the file from the source to destination (which is hasher here)
+	if _, err := io.Copy(hasher, file); err != nil {
+		return 0, err
+	}
+
+	// Sum64() returns the numeric hash directly
+	return hasher.Sum64(), nil
 }
